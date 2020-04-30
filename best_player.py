06486f34e54c359cc13e667dd650f3348bfbf3af
piotrstonka <piotr.stonka@o2.pl>
2020-04-30 11:17:47 +0200

    improve code readability and add comments

--- best_player.py
+++ best_player.py
@@ -1,76 +1,82 @@
-from hand_check import Hand_Check
-from copy import copy
+
 
 class Best_Player:
 
     hand_ranking = {
-    "high card":1,
-    "one pair": 2,
-    "two pairs":3,
-    "three of a kind":4,
-    "straight":5,
-    "flush":6,
-    "full house":7,
-    "four of a kind":8,
-    "straight flush":9,
-    "royal flush":10
+        "high card": 1,
+        "one pair": 2,
+        "two pairs": 3,
+        "three of a kind": 4,
+        "straight": 5,
+        "flush": 6,
+        "full house": 7,
+        "four of a kind": 8,
+        "straight flush": 9,
+        "royal flush": 10
     }
 
     rank_equivalents = {
-    2:2,
-    3:3,
-    4:4,
-    5:5,
-    6:6,
-    7:7,
-    8:8,
-    9:9,
-    10:10,
-    "J":11,
-    "Q":12,
-    "K":13,
-    "A":14
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14
     }
 
     def _init__(self):
         pass
 
-    def get_best_player(self, hand_check, common_cards, *players ):
-        """gets best player or players if split,
-         configuration of five cards (of random player if split)(?)
-         and name of configuration as a dictionary, where player is the key
-         and configuration and name of it in tuple as value
-          As input it takes five common cards
-          (not table instance) and a number of players. (not their hands)"""
+    def get_best_player(self, hand_check, common_cards, *players):
+        """
+        gets best player or players if split,
+        configuration of five cards
+        and name of configuration as a dictionary, where players are keys
+        and configuration and name of it in a tuple as value
+        As input it takes five common cards
+        (not table instance) and a number of player instances (not their hands).
+          """
 
         best_conf_and_ranking_for_each = self.get_conf_from_table_and_players(hand_check, common_cards, players)
-        # print(best_conf_and_ranking_for_each)
         all_configurations = [best_conf_and_ranking_for_each[player][1] for player in best_conf_and_ranking_for_each]
-        # print(all_configurations)
-        best_conf = max(all_configurations, key= lambda conf: self.hand_ranking[conf])
-        # print(best_conf)
-        players_with_best_conf = {key: value for (key, value) in best_conf_and_ranking_for_each.items() if value[1] == best_conf}
-        if len(players_with_best_conf) == 1:
-            return players_with_best_conf
-        i = 0
-        while i < 5 and len(players_with_best_conf) > 1:
+        best_conf = max(all_configurations, key=lambda conf: self.hand_ranking[conf])
+
+        #  filters dictionary of all players to just those with best configuration
+        players_with_best_conf = {key: value for (key, value) in best_conf_and_ranking_for_each.items()
+                                  if value[1] == best_conf}
+
+        #  if multiple players have the same configuration, compares highest card of each player, then
+        #  filters to those with same, highest cards, and repeats the process with all five cards if needed.
+        #  if c_num = 5, it means there are still multiple players in the players with best conf dict,
+        #  so a split is needed.
+        c_num = 0
+        while c_num < 5 and len(players_with_best_conf) > 1:
             list_of_ranks = []
             for player in players_with_best_conf:
-                list_of_ranks.append(players_with_best_conf[player][0][i].rank)
-            # print("start while loop")
-            # print(f"number of card: {i+ 1} ")
-            # print(f"dict: {players_with_best_conf}")
-            # print(f"length of dict: {len(players_with_best_conf)}")
-            # print(f"list of ranks:{list_of_ranks}")
+                list_of_ranks.append(players_with_best_conf[player][0][c_num].rank)
             highest_rank = max(list_of_ranks, key=lambda p: self.rank_equivalents[p])
-            # print(highest_rank)
-            players_with_best_conf = {key:value for (key,value) in players_with_best_conf.items() if value[0][i].rank == highest_rank}
-            i += 1
+            players_with_best_conf = {key: value for (key, value) in players_with_best_conf.items()
+                                      if value[0][c_num].rank == highest_rank}
+            c_num += 1
         return players_with_best_conf
 
 
 
     def get_conf_from_table_and_players(self, hand_check, common_cards, players):
+        """
+        returns a dictionary of all players as keys and corresponding configurations and
+        configuration names in tuples as values. Note that to access the configuration name
+        one has to use players index and [0] for configuration of cards
+        or [1] for configuration name, for example "one pair"
+        """
+
         output_dict = {}
         for player in players:
             all_cards = common_cards + player.hand
