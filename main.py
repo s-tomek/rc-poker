38495a006e9c92939084b19b8179f7b4dd1e1766
piotrstonka <piotr.stonka@o2.pl>
2020-05-07 17:52:26 +0200

    split main function to smaller functions

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ import os
 
 def main():
     game = Game()
+    game.set_parameters()
+    components = game.set_game()
+    game.play(*components)
 
 
 class Game:
 
     def __init__(self):
+
         self.players = []
         self.starting_sum = 0
         self.big_blind = 0
         self.blind_increase = 0
         self.mode = None
+
+    def set_parameters(self):
+        name_list = self.get_players()
+        self.get_sum()
+        self.get_big_blind()
+        self.get_mode()
+        self.init_players(name_list)
+
+    def get_players(self):
 
         os.system("clear")
         print("CHOOSE PLAYERS")
@@ -32,6 +45,9 @@ class Game:
                     break
                 else:
                     continue
+        return name_list
+
+    def get_sum(self):
 
         os.system("clear")
         print("CHOOSE STARTING SUM FOR PLAYER (max 100 000):", end="")
@@ -40,6 +56,8 @@ class Game:
                 self.starting_sum = int(input(""))
             except ValueError:
                 pass
+
+    def get_big_blind(self):
 
         os.system("clear")
         while 1 > self.big_blind or self.big_blind > self.starting_sum\
@@ -56,6 +74,8 @@ class Game:
             except ValueError:
                 pass
 
+    def get_mode(self):
+
         os.system("clear")
         possible_decisions = ["t", "c"]
         dec = None
@@ -67,18 +87,24 @@ class Game:
         elif dec == "t":
             self.mode = "transparent"
 
+    def init_players(self, name_list):
         for name in name_list:
             name = Player(name, self.starting_sum)
             self.players.append(name)
+
+    def set_game(self):
+
         deck = Deck()
         hand_check = Hand_Check()
         best_pl = Best_Player()
         table = Table(self.players, self.big_blind, self.blind_increase)
         r_man = RoundManager(self.mode)
-
         os.system("clear")
         print("game ready, click Enter to start", end="")
         input("")
+        return deck, table, r_man, best_pl, hand_check
+
+    def play(self, deck, table, r_man, best_pl, hand_check):
 
         for i in range(1000):
             r_man.new_cycle(deck, table)
